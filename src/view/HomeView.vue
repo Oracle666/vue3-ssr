613753ade75eab5ce4c7aea1d887cd87b9f05226
StasRyabtsev <ryabtsevstanislav2020@gmail.com>
2025-01-10 19:00:47 +0300

    init

---
+++ src/view/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const a = ref<number>(0)
+
+</script>
+
+<template>
+  <main>
+    <h1 @click="a++">{{ a }}</h1>
+    <div>111122213</div>
+  </main>
+</template>
